5aca4dfbb999b3834d28fcb2d9f49683d769377a
MCKoleman <manu25025@gmail.com>
2023-03-20 05:26:14 -0400

    Added sample users

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,1 +1,32 @@
-export default userList = []
+export type userList = [
+    {
+        name: "Dhinesh",
+        age: 24,
+        bio: "Need someone to go snowboarding with!",
+        img: "../assets/UserIcons/dhinesh24.png"
+    },
+    {
+        name: "Brandon",
+        age: 22,
+        bio: "D&D player, gamer, recreational wizard",
+        img: "../assets/UserIcons/brandon22.jfif"
+    },
+    {
+        name: "Hillary",
+        age: 18,
+        bio: "You may be cool, but you'll never be as cool as my hat",
+        img: "../assets/UserIcons/hillary18.jfif"
+    },
+    {
+        name: "Luka",
+        age: 23,
+        bio: "History buff, not very buff",
+        img: "../assets/UserIcons/luka23.jfif"
+    },
+    {
+        name: "Sarah",
+        age: 26,
+        bio: "Who's caught up in Game of Thrones?",
+        img: "../assets/UserIcons/sarah26.jfif"
+    },
+];
