c78be49fbe0c95cd926b914a64752a3d4eedbd06
MCKoleman <manu25025@gmail.com>
2023-03-20 23:56:39 -0400

    Added tailwind

--- src/data/users.ts
+++ src/data/users.ts
@@ -3,30 +3,147 @@ export type userList = [
         name: "Dhinesh",
         age: 24,
         bio: "Need someone to go snowboarding with!",
-        img: "../assets/UserIcons/dhinesh24.png"
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/dhinesh24.png",
+        imgs: []
     },
     {
         name: "Brandon",
         age: 22,
         bio: "D&D player, gamer, recreational wizard",
-        img: "../assets/UserIcons/brandon22.jfif"
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/brandon22.jfif",
+        imgs: []
     },
     {
         name: "Hillary",
         age: 18,
         bio: "You may be cool, but you'll never be as cool as my hat",
-        img: "../assets/UserIcons/hillary18.jfif"
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/hillary18.jfif",
+        imgs: []
     },
     {
         name: "Luka",
         age: 23,
         bio: "History buff, not very buff",
-        img: "../assets/UserIcons/luka23.jfif"
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/luka23.jfif",
+        imgs: []
     },
     {
         name: "Sarah",
         age: 26,
         bio: "Who's caught up in Game of Thrones?",
-        img: "../assets/UserIcons/sarah26.jfif"
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/sarah26.jfif",
+        imgs: []
+    },
+    {
+        name: "Rebecca",
+        age: 25,
+        bio: "I love watchin the Office with my cat",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/rebecca25.jfif",
+        imgs: []
+    },
+    {
+        name: "Alejandro",
+        age: 20,
+        bio: "Let's be friends!",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/alejandro20.jfif",
+        imgs: []
+    },
+    {
+        name: "Jared",
+        age: 19,
+        bio: "I never learned how to read",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/jared19.jfif",
+        imgs: []
+    },
+    {
+        name: "Pietro",
+        age: 21,
+        bio: "Avid bird enthusiast and park enjoyer",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/pietro21.jfif",
+        imgs: []
+    },
+    {
+        name: "Connor",
+        age: 24,
+        bio: "",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/connor24.jfif",
+        imgs: []
+    },
+    {
+        name: "Frank",
+        age: 22,
+        bio: "",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/frank22.png",
+        imgs: []
+    },
+    {
+        name: "Bridgit",
+        age: 19,
+        bio: "",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/bridgit19.jfif",
+        imgs: []
+    },
+    {
+        name: "Jere",
+        age: 27,
+        bio: "",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/jere27.jfif",
+        imgs: []
+    },
+    {
+        name: "Jamie",
+        age: 24,
+        bio: "",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/jamie24.jfif",
+        imgs: []
+    },
+    {
+        name: "Kylie",
+        age: 23,
+        bio: "",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/kylie23.jfif",
+        imgs: []
+    },
+    {
+        name: "Jeff",
+        age: 21,
+        bio: "Just another physics nerd looking to meet other cool nerdy people!",
+        interests: "",
+        lookingfor: "",
+        pfp: "../assets/UserIcons/jeff21.jfif",
+        imgs: [
+            "../assets/UserPictures/CollegeFun.jpeg",
+            "../assets/UserPictures/RocketLaunch.jpg"
+        ]
     },
 ];
