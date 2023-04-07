a7208cc10820936bd64e716b50710930d53a163a
MCKoleman <manu25025@gmail.com>
2023-04-07 15:37:02 -0400

    Allowed app to compile

--- src/app/matching-profile/matching-profile.component.ts
+++ src/app/matching-profile/matching-profile.component.ts
@@ -6,13 +6,25 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./matching-profile.component.css']
 })
 export class MatchingProfileComponent {
-  @Input() profile = {
-    name: "Sample",
-    age: 123,
-    bio: "Sample bio",
-    interests: "Sample interests",
-    lookingfor: "Sample looking",
-    pfp: "../assets/UserIcons/dhinesh24.png",
-    imgs: [] as string[]
-  };
+  @Input() profile: {
+    name: string,
+    age: number,
+    bio: string,
+    interests: string,
+    lookingfor: string,
+    pfp: string,
+    imgs: string[]
+  }
+  
+  constructor() {
+    this.profile = {
+      name: "Sample",
+      age: 123,
+      bio: "Sample bio",
+      interests: "Sample interests",
+      lookingfor: "Sample looking",
+      pfp: "../assets/UserIcons/dhinesh24.png",
+      imgs: [] as string[]
+    };
+  }
 }
